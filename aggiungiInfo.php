836117b1336bafceb7e31cbd483667e6396c82f1
patriziacorsiunipi <53034734+patriziacorsiunipi@users.noreply.github.com>
2023-08-23 15:47:07 +0200

    Add files via upload

--- aggiungiInfo.php
+++ aggiungiInfo.php
@@ -1,36 +1,52 @@
 <?php
-  //apro la sessione e includo la connessione al database
-  session_start();
-	include'connect.php';
-  $erroreReg = "false";
-  if (isset($_POST['idInfo']) && isset($_POST['info'])){
-    //Recupero i valori immessi nei campi di registrazione
-  	$info=mysqli_real_escape_string($conn,$_POST['info']);
-  	$idInfo = $_POST['idInfo'];
-    //faccio le query che mi serviranno per i confronti
-    $qIDInfo = "SELECT IdPersona FROM infoPersona WHERE IdPersona='$idInfo'";
-    $qIDInfoE = mysqli_query($conn,$qIDInfo);
-    $contaInfo = mysqli_num_rows($qIDInfoE);
-    //se l'id è già utilizzato stampo errore
-    if ($contaInfo>0){
-      $erroreReg = 'Informazione già presente';
+// Apri la sessione e includi la connessione al database
+session_start();
+include 'connect.php';
+
+$erroreReg = "false";
+
+if (isset($_POST['idInfo']) && isset($_POST['info'])) {
+    // Recupera i valori immessi nei campi di registrazione
+    $info = mysqli_real_escape_string($conn, $_POST['info']);
+    $idInfo = intval($_POST['idInfo']); // Converte in intero per evitare SQL injection
+
+    // Fai la query per verificare l'id
+    $qIDInfo = "SELECT IdPersona FROM infopersona WHERE IdPersona = ?";
+    $stmt = mysqli_prepare($conn, $qIDInfo);
+    mysqli_stmt_bind_param($stmt, "i", $idInfo);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result($stmt);
+    $contaInfo = mysqli_stmt_num_rows($stmt);
+
+    // Se l'id è già utilizzato stampa errore
+    if ($contaInfo > 0) {
+        $erroreReg = 'Informazione già presente';
     }
-    //se il nome è più lungo di 50 carattermi stampo errore
-    if (strlen($info)>1000){
-      $erroreReg = 'Lunghezza massima 1000 car.';
+    // Se la lunghezza dell'informazione supera i 1000 caratteri stampa errore
+    elseif (strlen($info) > 1000) {
+        $erroreReg = 'Lunghezza massima 1000 car.';
     }
-    //se non ci sono errori
-    if ($erroreReg=="false"){
-        //inserisco tutto nel database
-        $qRegistr = "INSERT INTO infopersona (IdPersona,Informazioni) VALUES ('$idInfo','$info')";
-        $qRegistrE = mysqli_query($conn,$qRegistr);
+
+    // Se non ci sono errori
+    if ($erroreReg == "false") {
+        // Inserisci tutto nel database utilizzando un prepared statement
+        $qRegistr = "INSERT INTO infopersona (IdPersona, Informazioni) VALUES (?, ?)";
+        $stmtInsert = mysqli_prepare($conn, $qRegistr);
+        mysqli_stmt_bind_param($stmtInsert, "is", $idInfo, $info);
+
+        if (mysqli_stmt_execute($stmtInsert)) {
+            echo $erroreReg;
+        } else {
+            echo "Errore durante l'inserimento dell'informazione.";
+        }
+
+        mysqli_stmt_close($stmtInsert);
+    } else {
         echo $erroreReg;
-      }else{
-        echo $erroreReg;
-      }
     }
 
+    mysqli_stmt_close($stmt);
+}
 
-
-  mysqli_close($conn);
+mysqli_close($conn);
 ?>
