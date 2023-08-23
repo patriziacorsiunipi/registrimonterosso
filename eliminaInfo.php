ec6e1ee3032e0bb27d465804e61af0322304b27f
patriziacorsiunipi <53034734+patriziacorsiunipi@users.noreply.github.com>
2023-08-23 16:23:08 +0200

    Add files via upload

--- eliminaInfo.php
+++ eliminaInfo.php
@@ -1,12 +1,23 @@
 <?php
-    //distruggo la sessione e indirizzo alla home
-    include 'connect.php';
-    session_start();
-    if (isset($_POST["idInfo"])){
-      $idInfo=$_POST["idInfo"];
-      $qEliminaInfo = "DELETE FROM infopersona WHERE IdPersona='$idInfo'";
-      $qEliminaInfoE = mysqli_query($conn,$qEliminaInfo);
-      $risposta="Messaggio eliminato";
-      echo $risposta;
+// Distruggo la sessione e indirizzo alla home
+include 'connect.php';
+session_start();
+
+if (isset($_POST["idInfo"])) {
+    $idInfo = $_POST["idInfo"];
+    
+    // Utilizza una query preparata per evitare SQL injection
+    $qEliminaInfo = "DELETE FROM infopersona WHERE IdPersona = ?";
+    $stmt = mysqli_prepare($conn, $qEliminaInfo);
+    mysqli_stmt_bind_param($stmt, "s", $idInfo);
+
+    if (mysqli_stmt_execute($stmt)) {
+        $risposta = "Messaggio eliminato";
+    } else {
+        $risposta = "Errore durante l'eliminazione del messaggio";
     }
+
+    mysqli_stmt_close($stmt);
+    echo $risposta;
+}
 ?>
