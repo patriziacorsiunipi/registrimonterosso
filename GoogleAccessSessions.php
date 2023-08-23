3a9f8792fb8eb83d7d21a3c8c1eb35d17f722f99
patriziacorsiunipi <53034734+patriziacorsiunipi@users.noreply.github.com>
2023-08-23 15:39:15 +0200

    Add files via upload

--- GoogleAccessSessions.php
+++ GoogleAccessSessions.php
@@ -1,27 +1,37 @@
 <?php
-include 'GoogleConfig.php';
-$login_button='';
-if (isset($_GET["code"])) {
-  $token = $google_client -> fetchAccessTokenWithAuthCode($_GET["code"]);
+session_start();
 
-  if (!isset($token["error"])){
-    $google_client->setAccessToken($token['access_token']);
-    $_SESSION["access_token"] = $token['access_token'];
-    $google_service = new Google_Service_Oauth2($google_client);
-    $data = $google_service->userinfo->get();
-    if (!empty($data["given_name"])){
-      $_SESSION['nome'] = $data['given_name'];
+// Filtra e valida il parametro "code"
+$authCode = filter_input(INPUT_GET, 'code', FILTER_SANITIZE_STRING);
+
+// Include il file di configurazione (assicurati che sia ben protetto)
+include 'GoogleConfig.php';
+
+$login_button = '';
+
+if (!empty($authCode)) {
+    $token = $google_client->fetchAccessTokenWithAuthCode($authCode);
+
+    if (!isset($token["error"])) {
+        $google_client->setAccessToken($token['access_token']);
+        $_SESSION["access_token"] = $token['access_token'];
+        $google_service = new Google_Service_Oauth2($google_client);
+        $data = $google_service->userinfo->get();
+
+        if (!empty($data["given_name"])) {
+            $_SESSION['nome'] = $data['given_name'];
+        }
+        if (!empty($data["family_name"])) {
+            $_SESSION['cognome'] = $data['family_name'];
+        }
+        if (!empty($data["email"])) {
+            $_SESSION['emailUtente'] = $data['email'];
+        }
+        $_SESSION['loginUtente'] = true;
     }
-    if (!empty($data["family_name"])){
-      $_SESSION['cognome'] = $data['family_name'];
-    }
-    if (!empty($data["email"])){
-      $_SESSION['emailUtente'] = $data['email'];
-    }
-    $_SESSION['loginUtente'] = "true";
-  }
 }
-if (!isset($_SESSION['access_token'])){
-  $login_button = '<a href="'.$google_client->createAuthUrl().'"><img class="img-fluid" src="img/btn_google.png" alt="Google Login"></a>';
+
+if (!isset($_SESSION['access_token'])) {
+    $login_button = '<a href="'.$google_client->createAuthUrl().'"><img class="img-fluid" src="img/btn_google.png" alt="Google Login"></a>';
 }
 ?>
