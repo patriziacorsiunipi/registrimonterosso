cfad8b4a1acd112a0259c947d2b57da58130c038
patriziacorsiunipi <53034734+patriziacorsiunipi@users.noreply.github.com>
2023-08-23 14:38:59 +0200

    Add files via upload

--- accesso.php
+++ accesso.php
@@ -1,34 +1,30 @@
 <?php
-  //includo il file di connessione al database e faccio partire la sessione
-  session_start();
-	include'connect.php';
-  $errore="false";
-  //se i campi nickname e password sono inseriti
-  $emailpost = $_POST['email'];
-  $passwordpost = $_POST['password'];
+session_start();
+include 'connect.php';
 
+if(isset($_POST['email']) && isset($_POST['password'])){
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = $_POST['password'];
 
-	if(isset($emailpost)&&isset($passwordpost)){
-    //registro le variabili nickname e password
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-    //cripto la password per questioni di sicurezza
-    $DecryptPassword = crypt($password, '$2a$07$cdacvdfgZZMALJsdrhaeQQQPPPDASFDGSDFG$');
-  	$qNickPass = "SELECT * FROM utente WHERE Email = '$email' AND Password = '$DecryptPassword' ";
-  	$qNickPassE = mysqli_query($conn,$qNickPass);
-  	$contaNickPass = mysqli_num_rows($qNickPassE);
-    $aNickPass = mysqli_fetch_array($qNickPassE);
-    //se esiste una riga contata
-  	if ($contaNickPass == 1) {
-      //apro la sessione Nickname, Login e, a seconda del tipo di utente, Autore o Utente
-      $_SESSION['emailUtente'] = $aNickPass[1];
-  		$_SESSION['nome'] = $aNickPass[2];
-  		$_SESSION['loginUtente'] = "true";
-  		echo($errore);
-  	}else { //se non esistono corrispondenze stampo un messaggio di errore
-      $errore="Nickname o password errati";
-      echo($errore);
-  	}
-	}
-  mysqli_close($conn);
+    $qNickPass = "SELECT * FROM utente WHERE Email = '$email'";
+    $qNickPassE = mysqli_query($conn, $qNickPass);
+
+    if(mysqli_num_rows($qNickPassE) == 1) {
+        $aNickPass = mysqli_fetch_array($qNickPassE);
+        $storedHash = $aNickPass['Password'];
+
+        if(password_verify($password, $storedHash)) {
+            $_SESSION['emailUtente'] = $aNickPass['Email'];
+            $_SESSION['nome'] = $aNickPass['Nome'];
+            $_SESSION['loginUtente'] = true;
+            echo "Success"; // Puoi anche evitare di inviare messaggi direttamente qui
+        } else {
+            echo "Nickname o password errati";
+        }
+    } else {
+        echo "Nickname o password errati";
+    }
+}
+
+mysqli_close($conn);
 ?>
