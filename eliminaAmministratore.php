6131901df446c010091a8e68f883de66f5cea08e
patriziacorsiunipi <53034734+patriziacorsiunipi@users.noreply.github.com>
2023-08-23 16:20:29 +0200

    Add files via upload

--- eliminaAmministratore.php
+++ eliminaAmministratore.php
@@ -1,12 +1,21 @@
 <?php
-    //distruggo la sessione e indirizzo alla home
-    include 'connect.php';
-    session_start();
-    if (isset($_POST["id"])){
-      $IDAmministratore=$_POST["id"];
-      $qEliminaUtente = "DELETE FROM amministratore WHERE IDAmministratore='$IDAmministratore'";
-      $qEliminaUtenteE = mysqli_query($conn,$qEliminaUtente);
-      $risposta="Utente eliminato";
-      echo $risposta;
+// Distruggo la sessione e indirizzo alla home
+include 'connect.php';
+session_start();
+
+if (isset($_POST["id"])) {
+    $IDAmministratore = $_POST["id"];
+    $qEliminaUtente = "DELETE FROM amministratore WHERE IDAmministratore = ?";
+    $stmt = mysqli_prepare($conn, $qEliminaUtente);
+    mysqli_stmt_bind_param($stmt, "s", $IDAmministratore);
+
+    if (mysqli_stmt_execute($stmt)) {
+        $risposta = "Utente eliminato";
+    } else {
+        $risposta = "Errore durante l'eliminazione dell'utente";
     }
+
+    mysqli_stmt_close($stmt);
+    echo $risposta;
+}
 ?>
