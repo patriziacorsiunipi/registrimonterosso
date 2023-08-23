bd466a0dde4b7e255e9abea68cfbe2b929f1f717
patriziacorsiunipi <53034734+patriziacorsiunipi@users.noreply.github.com>
2023-08-23 16:03:17 +0200

    Add files via upload

--- aggiungiUtente.php
+++ aggiungiUtente.php
@@ -1,51 +1,68 @@
 <?php
-  //apro la sessione e includo la connessione al database
-  session_start();
-	include'connect.php';
-  $erroreReg = "false";
-  if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['password']) ){
-    //Recupero i valori immessi nei campi di registrazione
-  	$nome=mysqli_real_escape_string($conn,$_POST['nome']);
-  	$password = $_POST['password'];
-  	$email = strtolower($_POST['email']);
-    //faccio le query che mi serviranno per i confronti
-    $qEmail = "SELECT Email FROM utente WHERE Email='$email'";
-    $qEmailE = mysqli_query($conn,$qEmail);
-    $contaEmail = mysqli_num_rows($qEmailE);
-    //se il nome è più lungo di 50 carattermi stampo errore
-    if (strlen($nome)>50){
-      $erroreReg = 'Lunghezza massima 50 car.';
+// Apri la sessione e includi la connessione al database
+session_start();
+include 'connect.php';
+
+$erroreReg = "false";
+
+if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['password'])) {
+    // Recupera i valori immessi nei campi di registrazione
+    $nome = mysqli_real_escape_string($conn, $_POST['nome']);
+    $password = $_POST['password'];
+    $email = strtolower($_POST['email']);
+
+    // Fai le query che ti serviranno per i confronti
+    $qEmail = "SELECT Email FROM utente WHERE Email = ?";
+    $stmtEmail = mysqli_prepare($conn, $qEmail);
+    mysqli_stmt_bind_param($stmtEmail, "s", $email);
+    mysqli_stmt_execute($stmtEmail);
+    mysqli_stmt_store_result($stmtEmail);
+    $contaEmail = mysqli_stmt_num_rows($stmtEmail);
+
+    // Se il nome è più lungo di 50 caratteri stampa errore
+    if (strlen($nome) > 50) {
+        $erroreReg = 'Lunghezza massima 50 car.';
     }
-    //se il nome contiene numeri stampo errore
-    if(preg_match('/[0-9+-:,;.?!"£$%&(){}=]/i', $nome)){
-      $erroreReg = 'Nome non valido';
+    // Se il nome contiene caratteri non validi stampa errore
+    elseif (!preg_match('/^[a-zA-Z\s]+$/', $nome)) {
+        $erroreReg = 'Nome non valido';
     }
-    //se l'email è già utilizzata stampo errore
-    if ($contaEmail>0){
-      $erroreReg = 'Email già utilizzata';
+    // Se l'email è già utilizzata stampa errore
+    elseif ($contaEmail > 0) {
+        $erroreReg = 'Email già utilizzata';
     }
-    //se email non è valida stampo errore
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $erroreReg = 'Email non valida!';
+    // Se l'email non è valida stampa errore
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $erroreReg = 'Email non valida!';
     }
-    //se la password è minore di 8 o maggiore di 12 caratteri stampo errore
-    if(strlen($password)<8||strlen($password)>12){
-      $erroreReg = 'Password non valida! Min:8 caratteri - Max:12 caratteri';
+    // Se la password è minore di 8 o maggiore di 12 caratteri stampa errore
+    elseif (strlen($password) < 8 || strlen($password) > 12) {
+        $erroreReg = 'Password non valida! Min: 8 caratteri - Max: 12 caratteri';
     }
-    //se non ci sono errori
-    if ($erroreReg=="false"){
-        //cripto la password inserita
-        $cryptPassword=crypt($_POST['password'], '$2a$07$usesomesillystringforsalt$');
-        //inserisco tutto nel database
-        $qRegistr = "INSERT INTO utente (Email,Nome,Password) VALUES ('$email','$nome','$cryptPassword')";
-        $qRegistrE = mysqli_query($conn,$qRegistr);
+
+    // Se non ci sono errori
+    if ($erroreReg == "false") {
+        // Hash della password con password_hash()
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        // Inserisci tutto nel database utilizzando un prepared statement
+        $qRegistr = "INSERT INTO utente (Email, Nome, Password) VALUES (?, ?, ?)";
+        $stmtInsert = mysqli_prepare($conn, $qRegistr);
+        mysqli_stmt_bind_param($stmtInsert, "sss", $email, $nome, $hashedPassword);
+
+        if (mysqli_stmt_execute($stmtInsert)) {
+            echo $erroreReg;
+        } else {
+            echo "Errore durante la registrazione.";
+        }
+
+        mysqli_stmt_close($stmtInsert);
+    } else {
         echo $erroreReg;
-      }else{
-        echo $erroreReg;
-      }
     }
 
+    mysqli_stmt_close($stmtEmail);
+}
 
-
-  mysqli_close($conn);
+mysqli_close($conn);
 ?>
